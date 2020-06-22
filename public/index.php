8023a13bd1a4ac0b90a769659e253e4ed3e477ed
Magdalena Slomiany <magdalena.slomiany.programista@gmail.com>
2020-06-22 10:28:50 +0200

    added class autoloader

--- public/index.php
+++ public/index.php
@@ -1,7 +1,15 @@
 <?php
 
-require '../Core/Router.php';
-require '../App/Controllers/Home.php';
+spl_autoload_register(function ($class) {
+    
+	$root = dirname(__DIR__);
+    $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
+	
+    if (is_readable($file)) {
+		
+        require $root . '/' . str_replace('\\', '/', $class) . '.php';
+    }
+});
 	
 $router = new Core\Router();
 
