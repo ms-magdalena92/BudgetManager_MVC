1a17dda96c99ac3465949da36e38aeceda18e295
Magdalena Slomiany <magdalena.slomiany.programista@gmail.com>
2020-06-22 12:39:56 +0200

    action filters test

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -4,8 +4,18 @@ namespace App\Controllers;
 
 class Home extends \Core\Controller
 {
-    public function index()
+    public function indexAction()
     {
         echo 'Home controller';
     }
+	
+	protected function before()
+	{
+	    echo "(before action filter) ";
+	}
+	
+	protected function after()
+	{
+	    echo " (after action filter)";
+	}
 }
