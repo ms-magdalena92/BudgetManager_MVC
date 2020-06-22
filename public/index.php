2283e82e3f3c9b170073f8bb0f22014885c91a47
Magdalena Slomiany <magdalena.slomiany.programista@gmail.com>
2020-06-22 14:17:47 +0200

    added composer loader updated with automatic class loading

--- public/index.php
+++ public/index.php
@@ -1,15 +1,6 @@
 <?php
 
-spl_autoload_register(function ($class) {
-    
-	$root = dirname(__DIR__);
-    $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
-	
-    if (is_readable($file)) {
-		
-        require $root . '/' . str_replace('\\', '/', $class) . '.php';
-    }
-});
+require_once dirname(__DIR__).'/vendor/autoload.php';
 	
 $router = new Core\Router();
 
@@ -17,14 +8,3 @@ $router -> addRoute('{controller}/{action}');
 	
 $url = $_SERVER['QUERY_STRING'];
 $router -> dispatchRoute($url);
-
-/*if ($router -> matchUrlToRoutes($url)) {
-	
-	echo '<pre>';
-	var_dump($router -> getRouteParams());
-	echo '</pre>';
-	
-} else {
-	
-	echo "No route found for URL '$url'";
-}*/
