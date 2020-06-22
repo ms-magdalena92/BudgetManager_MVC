93e052b153a145ced0acfece9eeb78649d680d2d
Magdalena Slomiany <magdalena.slomiany.programista@gmail.com>
2020-06-22 15:46:25 +0200

    added twig template rendering

--- Core/View.php
+++ Core/View.php
@@ -19,4 +19,17 @@ class View
             throw new \Exception("$file not found");
         }
     }
+	
+    public static function renderTemplate($template, $args = [])
+    {
+        static $twig = null;
+
+        if ($twig === null) {
+			
+            $loader = new \Twig\Loader\FilesystemLoader('../App/Views');
+            $twig = new \Twig\Environment($loader);
+        }
+		
+        echo $twig->render($template, $args);
+    }
 }
