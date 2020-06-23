12b1df67d437c585dca2c651af6bf494bf322400
Magdalena Slomiany <magdalena.slomiany.programista@gmail.com>
2020-06-23 16:00:27 +0200

    added homepage route

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require_once dirname(__DIR__).'/vendor/autoload.php';
 	
 $router = new Core\Router();
 
+$router -> addRoute('', ['controller' => 'Home', 'action' => 'index']);
 $router -> addRoute('{controller}/{action}');
 	
 $url = $_SERVER['QUERY_STRING'];
