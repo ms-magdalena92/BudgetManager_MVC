a78d6c815d54993595c8411ceab4fe8ea367a66f
Magdalena Slomiany <magdalena.slomiany.programista@gmail.com>
2020-06-24 10:23:21 +0200

    set error and exceptions handlers in front controller

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
 
 require_once dirname(__DIR__).'/vendor/autoload.php';
+
+error_reporting(E_ALL);
+set_error_handler('Core\Error::errorHandler');
+set_exception_handler('Core\Error::exceptionHandler');
 	
 $router = new Core\Router();
 
