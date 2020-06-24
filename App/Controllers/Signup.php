e7fa2374d32f2fa71ec785b6582feee78d91763e
Magdalena Slomiany <magdalena.slomiany.programista@gmail.com>
2020-06-24 12:17:29 +0200

    signup controller

---
+++ App/Controllers/Signup.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+
+class Signup extends \Core\Controller
+{
+    public function newAction()
+    {
+        View::renderTemplate('Signup/new.html');
+    }
+	
+    public function createAction()
+    {
+    }
+}
