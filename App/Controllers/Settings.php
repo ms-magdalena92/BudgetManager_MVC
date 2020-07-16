b5531e58e4267686676a5b1f0a734c68b71bcfe8
Magdalena Slomiany <magdalena.slomiany.programista@gmail.com>
2020-07-16 13:16:05 +0200

    income categories settings page

--- App/Controllers/Settings.php
+++ App/Controllers/Settings.php
@@ -3,11 +3,21 @@
 namespace App\Controllers;
 
 use \Core\View;
+use \App\Models\Categories;
 
 class Settings extends Authenticated
 {
     public function profileAction()
     {
         View::renderTemplate('Settings/profile.html');
+    }
+
+    public function incomeCategoriesAction()
+    {
+        $incomeCategories = Categories::getCurrentUserIncomeCategories();
+
+        View::renderTemplate('Settings/income-categories.html', [
+            'incomeCategories' => $incomeCategories
+        ]);
     }
 }
